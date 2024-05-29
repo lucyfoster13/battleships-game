1f155e160c579e80c8cf5e4c0a2833fe936feca0
Lucy Foster <lucyfoster13@gmail.com>
2024-05-29 16:55:57 +0000

    Outline game functions

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+LENGTH_OF_SHIPS = [2,3,3,4,5]  
+PLAYER_BOARD = [[" "] * 8 for i in range(8)]
+COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
+PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
+COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
+LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+def print_board(board):
+    pass
+
+def place_ships(board):
+    pass
+
+def check_ship_fit():
+    pass
+
+def ship_overlaps():
+    pass
+
+def user_input():
+    pass
+
+def count_hit_ships():
+    pass
+
+def turn(board):
+    pass
+
+while True:
+    
