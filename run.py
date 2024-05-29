d759dbaa16acc60ef01edc7719bb5dbe9a536ee2
Lucy Foster <lucyfoster13@gmail.com>
2024-05-29 20:20:06 +0000

    Add welcome message

--- run.py
+++ run.py
@@ -8,6 +8,9 @@ COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
 LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 def print_board(board):
+    print("*****************************")
+    print("***Welcome to Battleships!***")
+    print("*****************************")
     print("  A B C D E F G H")
     print("  +-+-+-+-+-+-+-+")
     row_number = 1
